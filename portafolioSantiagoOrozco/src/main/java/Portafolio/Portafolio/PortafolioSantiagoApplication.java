001fd774472aaa407dc65ff610162cb5bd48b7aa
santiOM0611 <sorozco80826@ufide.ac.cr>
2025-01-22 20:55:58 -0600

    Semana2
    Trabajo en clase

---
+++ portafolioSantiagoOrozco/src/main/java/Portafolio/Portafolio/PortafolioSantiagoApplication.java
@@ -0,0 +1,13 @@
+package Portafolio.Portafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioSantiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioSantiagoApplication.class, args);
+	}
+
+}
---
+++ portafolioSantiagoOrozco/src/test/java/Portafolio/Portafolio/PortafolioSantiagoApplicationTests.java
@@ -0,0 +1,13 @@
+package Portafolio.Portafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioSantiagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
