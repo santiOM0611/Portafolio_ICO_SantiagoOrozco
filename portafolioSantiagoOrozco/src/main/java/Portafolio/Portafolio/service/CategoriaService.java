cb3504123d9bdaf459054edf1bdae34b1a44a102
santiOM0611 <sorozco80826@ufide.ac.cr>
2025-02-20 11:29:25 -0600

    Semana 6
    Presentación semana #5

---
+++ portafolioSantiagoOrozco/src/main/java/Portafolio/Portafolio/service/CategoriaService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package Portafolio.Portafolio.service;
+
+import Portafolio.Portafolio.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+ 
+}
---
+++ portafolioSantiagoOrozco/src/main/java/Portafolio/Portafolio/service/impl/CategoriaServiceImpl.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Portafolio.Portafolio.service.impl;
+
+import Portafolio.Portafolio.dao.CategoriaDao;
+import Portafolio.Portafolio.domain.Categoria;
+import Portafolio.Portafolio.service.CategoriaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+    
+    @Autowired
+    private CategoriaDao categoriaDao;
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista=categoriaDao.findAll();
+        if (activos) {
+           lista.removeIf(e -> !e.isActivo());
+        }
+        return lista;
+    }
+}
