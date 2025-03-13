a90d542c3db967c7ef022629b5ffa41dbec5ad1c
santiOM0611 <sorozco80826@ufide.ac.cr>
2025-03-12 19:27:21 -0600

    Semana 9
    Consultas Ampliadas

--- portafolioSantiagoOrozco/src/main/java/Portafolio/Portafolio/service/impl/ProductoServiceImpl.java
+++ portafolioSantiagoOrozco/src/main/java/Portafolio/Portafolio/service/impl/ProductoServiceImpl.java
@@ -46,4 +46,24 @@ public class ProductoServiceImpl implements ProductoService{
     public void delete(Producto producto) {
         productoDao.delete(producto);
     }
+    
+    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup) {
+        return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> metodoJPQL(double precioInf, double precioSup) {
+        return productoDao.metodoJPQL(precioInf, precioSup);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> metodoNativo(double precioInf, double precioSup) {
+        return productoDao.metodoNativo(precioInf, precioSup);
+    }
+
 }
