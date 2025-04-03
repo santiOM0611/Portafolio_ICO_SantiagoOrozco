68b5236173bc174d66475232eb6c23973dd6ed8b
santiOM0611 <sorozco80826@ufide.ac.cr>
2025-04-02 20:44:42 -0600

    Semana #11
     CRUD de usuarios

---
+++ portafolioSantiagoOrozco/src/main/java/Portafolio/Portafolio/dao/RolDao.java
@@ -0,0 +1,8 @@
+package Portafolio.Portafolio.dao;
+
+import Portafolio.Portafolio.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
